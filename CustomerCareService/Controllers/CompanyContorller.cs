dff89a70e68f6e7c6fa6d6c7280b51edde99d2a9
akgolali <akgol97_@hotmail.com>
2024-04-29 22:34:57 -0400

    Compoany Controller is added too

---
+++ CustomerCareService/Controllers/CompanyContorller.cs
@@ -0,0 +1,47 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTO;
+using Shared.Repositories;
+
+namespace CustomerCareService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyContorller: ControllerBase
+    {
+
+
+        private readonly DatabaseContext _databaseContext;
+        
+        private readonly CompanyRepository _companyRepository;
+
+        public CompanyContorller(CompanyRepository companyRepository, DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+            _companyRepository = companyRepository;
+        }
+
+
+        [HttpPost]  
+
+        public async Task<IActionResult> AddCompany(CompanyDTO company)
+        {
+            if( await _companyRepository.CompanyExists(company.CompanyCode) )
+            {
+                return Conflict("The company code is exist");
+            }
+
+            var newCompany= new Company()
+                {
+                CompanyCode = company.CompanyCode,
+                CompanyName = company.CompanyName,
+            };
+
+            await _companyRepository.AddAsync(newCompany);
+            return Ok();
+
+        }
+
+    }
+}
