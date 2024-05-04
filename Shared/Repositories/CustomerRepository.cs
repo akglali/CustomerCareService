5f82acd1b239052ee2a27691d0593a5b89cc7150
akgolali <akgol97_@hotmail.com>
2024-05-04 00:41:23 -0400

    customers can be added and can be found via phone number

---
+++ Shared/Repositories/CustomerRepository.cs
@@ -0,0 +1,50 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using SendGrid.Helpers.Errors.Model;
+using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Repositories
+{
+    public class CustomerRepository (DatabaseContext context) : BaseRepository<Customer>(context)
+    {
+         
+        public async Task<bool> CustomerExist(string Phone)
+        {
+            return await context.Customers.AnyAsync(c=> c.CustomerPhone == Phone);
+        }
+
+
+        public async Task<CustomerDetailDTO> GetCustomerDetail(string phone)
+        {
+
+            var customer = await context.Customers.Include(o => o.Office).ThenInclude(c=>c.Company).FirstOrDefaultAsync(c=> c.CustomerPhone == phone);
+
+
+            return new CustomerDetailDTO()
+            {
+                FirstName= customer.FirstName,
+                LastName= customer.LastName,
+                Email= customer.CustomerEmail,
+                PhoneNumber= customer.CustomerPhone,
+                Office= new OfficeDTOWithAllEntities()
+                {
+                  Name= customer.Office.OfficeName,
+                  OfficeCode=customer.Office.OfficeCode,
+                  CreatedDate=customer.Office.CreatedDate,
+                    Company = new AddCompanyDTO()
+                    {
+                        CompanyCode = customer.Office.Company.CompanyCode,
+                        CompanyName = customer.Office.Company.CompanyName,
+                    }
+                }
+            };
+
+        }
+
+    }
+}
