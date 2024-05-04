a2a3e89182924df8ceea02e3ac001a53e2f5bc7b
akgolali <akgol97_@hotmail.com>
2024-05-04 00:40:17 -0400

    user can get all companies detail with one request

--- Shared/Repositories/CompanyRepository.cs
+++ Shared/Repositories/CompanyRepository.cs
@@ -5,30 +5,26 @@ using Shared.DTO;
 
 namespace Shared.Repositories
 {
-    public class CompanyRepository: BaseRepository<Company>
+    public class CompanyRepository (DatabaseContext context): BaseRepository<Company>(context)
     {
-        private readonly DatabaseContext _databaseContext;
-        public CompanyRepository(DatabaseContext context) : base(context)
-        {
-            _databaseContext = context;
-        }
+      
 
         public async Task<bool> CompanyExists(int companyCode)
         {
-            return await _databaseContext.Companies.AnyAsync(c => c.CompanyCode == companyCode);
+            return await context.Companies.AnyAsync(c => c.CompanyCode == companyCode);
 
         }
 
 
         public async Task<Company?> GetCompanyId(int CompanyCode)
         {
-            return await _databaseContext.Companies.FirstOrDefaultAsync(c => c.CompanyCode == CompanyCode);
+            return await context.Companies.FirstOrDefaultAsync(c => c.CompanyCode == CompanyCode);
         }
 
         public async Task<CompanyDTO> CompanyByCode(int CompanyCode)
         {
 
-            var company= await _databaseContext.Companies.Include(c => c.Offices).FirstOrDefaultAsync(c => c.CompanyCode == CompanyCode);
+            var company= await context.Companies.Include(c => c.Offices).FirstOrDefaultAsync(c => c.CompanyCode == CompanyCode);
 
             return new CompanyDTO(){
                 CompanyCode = company.CompanyCode,
@@ -39,7 +35,34 @@ namespace Shared.Repositories
                 
                 }).ToList()
             };
-            
+        }
+
+        public async Task<List<CompanyDTO>> GetAllCompanies()
+        {
+
+            var companies= await context.Companies.Include(c => c.Offices).ToListAsync();
+
+            var allCompanies = new List<CompanyDTO>();
+
+
+            foreach(var company in companies)
+            {
+                allCompanies.Add(new CompanyDTO
+                {
+                    CompanyCode = company.CompanyCode,
+                    CompanyName = company.CompanyName,
+                    Offices = company.Offices!.Select(o => new OfficeCompanyDTO
+                    {
+                        Name = o.OfficeName,
+                        OfficeCode = o.OfficeCode,
+
+                    }).ToList()
+                });
+            }
+
+
+           
+            return allCompanies;
 
         }
     }
