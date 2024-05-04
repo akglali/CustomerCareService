cbb1267e1608edd4c0f977dd54cc94d78f2189e5
akgolali <akgol97_@hotmail.com>
2024-05-04 00:41:02 -0400

    now user can get all office details

--- Shared/DTO/OfficeDTO.cs
+++ Shared/DTO/OfficeDTO.cs
@@ -8,9 +8,23 @@ namespace Shared.DTO
 {
     public class OfficeDTO
     {
-        public int OfficeCode {  get; set; }
+        public int OfficeCode { get; set; }
         public string Name { get; set; }
 
-        public int CompanyCode {  get; set; }
+        public int CompanyCode { get; set; }
     }
+
+    public class OfficeDTOWithAllEntities
+    {
+        public int OfficeCode { get; set; }
+        public string Name { get; set; }
+
+        public AddCompanyDTO Company { get; set; }
+
+        public DateTime CreatedDate { get; set; }   
+
+        //public List<CustomerDTO> Customers { get; set; }
+
+    }
+
 }
--- Shared/Repositories/OfficeRepository.cs
+++ Shared/Repositories/OfficeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Shared.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +9,66 @@ using System.Threading.Tasks;
 
 namespace Shared.Repositories
 {
-    
+
     public class OfficeRepository(DatabaseContext context) : BaseRepository<Office>(context)
     {
 
-    
+
         //check if the office exist
         public async Task<bool> OfficeExist(int OfficeCode)
         {
-            return await  context.Offices.AnyAsync(O => O.OfficeCode == OfficeCode);
+            return await context.Offices.AnyAsync(o => o.OfficeCode == OfficeCode);
+        }
+
+
+        public async Task<Office?> GetOfficeById(int OfficeCode)
+        {
+            return await context.Offices.FirstOrDefaultAsync(o => o.OfficeCode == OfficeCode);
+        }
+
+        public async Task<List<OfficeDTOWithAllEntities>> GetAllOffice()
+        {
+            var offices = await context.Offices.Include(o=> o.Company).ToListAsync();
+
+
+            var allOffice = new List<OfficeDTOWithAllEntities>();
+          
+            foreach (var office in offices)
+            {
+                allOffice.Add(new OfficeDTOWithAllEntities()
+                {
+                    OfficeCode = office.OfficeCode,
+                    Name = office.OfficeName,
+                    CreatedDate = DateTime.Now,
+                    Company = new AddCompanyDTO()
+                    {
+                        CompanyCode = office.Company.CompanyCode,
+                        CompanyName = office.Company.CompanyName,
+                    },
+
+                });
+            }
+
+            return allOffice;
+        }
+
+        public async Task<OfficeDTOWithAllEntities> GetOfficeByCode(int OfficeCode)
+        {
+
+            var office = await context.Offices.Include(o => o.Company).FirstOrDefaultAsync(o => o.OfficeCode == OfficeCode);
+
+            return new OfficeDTOWithAllEntities()
+            {
+                OfficeCode = office.OfficeCode,
+                Name = office.OfficeName,
+                CreatedDate = DateTime.Now,
+                Company = new AddCompanyDTO()
+                {
+                    CompanyCode = office.Company.CompanyCode,
+                    CompanyName = office.Company.CompanyName,
+                },
+            };
+        
         }
     }
 }
