66cfbd9ad7faa58ebaa8c03fe18615a25a9c78f4
akgolali <akgol97_@hotmail.com>
2024-05-04 00:37:17 -0400

    customer dtos are added

---
+++ Shared/DTO/CustomerDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public class CustomerDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email {  get; set; }
+
+        public int OfficeCode { get; set; }   
+
+    }
+
+
+    public class CustomerDetailDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public OfficeDTOWithAllEntities Office { get; set; }
+
+        //public List<CustomerCaseDTO> CustomerCases {get;set;}
+    }
+}
